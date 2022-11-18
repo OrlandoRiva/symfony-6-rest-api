ff3bd4560ed35f9fecd61fa2c731ab71f3948573
OrlandoRiva <orlando.carvalho@riva-office.pt>
2022-11-18 13:08:16 +0000

    bootstrap and css

--- assets/react/controllers/MyComponent.jsx
+++ assets/react/controllers/MyComponent.jsx
@@ -6,6 +6,7 @@ export default function () {
     const [count, setCount] = useState();
     const [last, setLast] = useState(3);
     const [remove, setRemove] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios
@@ -14,6 +15,7 @@ export default function () {
                 setProjects(res.data.projects);
                 setCount(res.data.count)
                 setRemove(false);
+                setLoading(false);
             })
             .catch((err) => {
                 // console.log(err);
@@ -33,22 +35,26 @@ export default function () {
 
     return (
         <>
-            <div>
-                <h2>Count {count}</h2>
-                <button onClick={() => setLast(3)}>3</button>
-                <button onClick={() => setLast(10)}>10</button>
-                <button value="" onClick={(e) => setLast(e.target.value)}>All</button>
-                {projects.sort((a, b) => a.id > b.id ? 1 : -1).map((project) => (
-                    <div key={project.id}>
-                        <h1>{project.name} {project.id}</h1>
-                        <p>{project.description}</p>
-                        <span><b>Theme:</b> {project.theme.name}</span>
-                    </div>
-                ))}
-                <br/>
-                <hr/>
-                <button onClick={handleDelete}>DELETE LAST PROJECT</button>
+            <h1 className="mt-5">React Test</h1>
+            <h2 className="mt-5">Count Projects: {count}</h2>
+            <h3 className="mt-5">Filter:</h3>
+            <div className="btn-group">
+                <button className="btn btn-outline-primary" onClick={() => setLast(3)}>3</button>
+                <button className="btn btn-outline-primary" onClick={() => setLast(10)}>10</button>
+                <button className="btn btn-outline-primary" value="" onClick={(e) => setLast(e.target.value)}>All
+                </button>
             </div>
+            {!loading && projects.sort((a, b) => a.id > b.id ? 1 : -1).map((project) => (
+                <div className="mt-5" key={project.id}>
+                    <h1 className="text-capitalize text-success">{project.name} ({project.id})</h1>
+                    <p>{project.description}</p>
+                    <span className="text-capitalize"><b
+                        className="text-secondary">Theme:</b> {project.theme.name}</span>
+                </div>
+            ))}
+            <br/>
+            <hr/>
+            <button className="btn btn-danger" onClick={handleDelete}>DELETE LAST PROJECT</button>
         </>
     );
 }
